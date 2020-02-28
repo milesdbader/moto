33dcf7083082475c91b894d6ecaa07a7f8843f29
Eyal Cohen <eyalcohen2524@gmail.com>
2020-02-28 07:59:35 -0800

    i am close but no cigar

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,25 @@
 class VotesController < ApplicationController
   before_action :set_challenge, only: %i[new]
   def create
+    @vote = Vote.new(vote_params)
+    @vote.user = current_user
+    @vote.save
+    # redirect_to new_challenge_vote(current_user.next_votable_challenge.id)
+    redirect_to root
   end
 
   def index
   end
 
   def new
+    @vote = Vote.new
   end
 
   private
+
+  def vote_params
+    params.require(:vote).permit(:player)
+  end
 
   def set_challenge
     @challenge = Challenge.find(params[:challenge_id])
