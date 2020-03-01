be3a1bf292ff4c1f165d44414d051e45de8121f3
Miles Bader <milesdbader@gmail.com>
2020-03-01 12:47:12 +0200

    added sweet alert for rejecting a challenge

--- app/javascript/packs/swal.js
+++ app/javascript/packs/swal.js
@@ -1,17 +1,34 @@
 import swal from 'sweetalert';
 
-const initSweetalert = (selector, options = {}) => {
+const initSweetalert = (selector, options = {}, callback = () => {}) => {
   const swalButton = document.querySelector(selector);
   if (swalButton) { // protect other pages
     swalButton.addEventListener('click', () => {
-      swal(options);
+      swal(options).then(callback);
     });
   }
 };
 
 
-initSweetalert('#sweet-alert-demo', {
-  title: "A nice alert",
-  text: "This is a great alert, isn't it?",
-  icon: "success"
-});
+// Setup Sweet Alert for 'cancel challenge' buttons
+document.querySelectorAll('.btn-challenge-cancel').forEach((btn) => {
+  const challengeId = btn.dataset.challengeId;
+  const selector = `.btn-challenge-cancel[data-challenge-id="${challengeId}"`;
+  const callback = (value) => {
+    if (value) {
+      const link = btn.parentElement.querySelector('.reject-link');
+      // link.click();
+      console.log('about to click: ')
+      console.log(link)
+    }
+  }
+
+  initSweetalert(selector,
+    {
+      title: "Are you sure?",
+      text: "This action cannot be reversed",
+      icon: "warning"
+    },
+    callback
+  )
+})
