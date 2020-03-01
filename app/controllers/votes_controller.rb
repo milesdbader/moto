dcc0644438335200aba1145318ff83d1349b8e7a
Eyal Cohen <eyalcohen2524@gmail.com>
2020-03-01 02:14:33 -0800

    fixed most changes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,10 +2,10 @@ class VotesController < ApplicationController
   before_action :set_challenge, only: %i[new]
   def create
     @vote = Vote.new(player: Player.find(params[:player]), user: current_user)
-    @vote.user = current_user
     @vote.save
     # redirect_to new_challenge_vote(current_user.next_votable_challenge.id)
-    redirect_to root_path
+    redirect_to new_challenge_vote(current_user.next_votable_challenge)
+
   end
 
   def index
