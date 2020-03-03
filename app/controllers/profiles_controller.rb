29207f831d7a768faa526366ccee19d8f150cc4f
David Stranders <david.stranders@gmail.com>
2020-03-03 17:36:22 +0200

    Made the user edit page route backend - just need to do design now

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,22 @@ class ProfilesController < ApplicationController
 
   def show
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+   if current_user.update(strong_params)
+      redirect_to profile_path(current_user)
+    else
+      render :edit
+   end
+  end
+
+  private
+
+  def strong_params
+    params.require(:user).permit(:username, :photo)
+  end
 end
